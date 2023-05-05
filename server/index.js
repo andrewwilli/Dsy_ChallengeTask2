0da1ac00ed7b25e2fd1fce91822dc064ffe2b393
Andrew Willi <andrew.willi@ost.ch>
2023-05-05 18:27:39 +0200

    f

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(cors());
@@ -34,7 +34,7 @@ app.post("/todos", async (req, res) => {
   res.json(todo);
 });
 
-const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
