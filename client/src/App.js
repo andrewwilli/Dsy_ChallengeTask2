37e721e061ba783714e9b8a78ddbd80b469cdeaf
Andrew Willi <andrew.willi@ost.ch>
2023-05-05 16:08:41 +0200

    proxy not working

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,73 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  const [data, setData] = React.useState(null);
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
 
-  React.useEffect(() => {
-    fetch("/api")
-        .then((res) => res.json())
-        .then((data) => setData(data.message));
+  useEffect(() => {
+    async function fetchTodos() {
+      const response = await axios.get("/api/todos");
+      setTodos(response.data);
+    }
+    fetchTodos();
   }, []);
 
+  async function addTodo() {
+    const todo = { title: newTodo, completed: false };
+    const response = await axios.post("/api/todos", todo);
+    setTodos([...todos, response.data]);
+    setNewTodo("");
+  }
+
+  function handleNewTodoChange(event) {
+    setNewTodo(event.target.value);
+  }
+
   return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{!data ? "Loading..." : data}</p>
-        </header>
-      </div>
+    <div>
+      <h1>My Todo List</h1>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/new">New Todo</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home todos={todos} />} />
+          <Route path="/new" element={<NewTodo newTodo={newTodo} handleNewTodoChange={handleNewTodoChange} addTodo={addTodo} />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+function Home({ todos }) {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo._id}>
+          {todo.title} - {todo.completed ? "Completed" : "Incomplete"}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function NewTodo({ newTodo, handleNewTodoChange, addTodo }) {
+  return (
+    <div>
+      <input type="text" value={newTodo} onChange={handleNewTodoChange} />
+      <button onClick={addTodo}>Add Todo</button>
+    </div>
   );
 }
 
 export default App;
+
--- server/index.js
+++ server/index.js
@@ -1,13 +1,40 @@
 const express = require("express");
-
-const PORT = process.env.PORT || 3001;
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Fuck you!" });
+app.use(cors());
+app.use(express.json());
+
+const todoSchema = new mongoose.Schema({
+  title: String,
+  completed: Boolean,
 });
 
+const Todo = mongoose.model("Todo", todoSchema);
+
+const MONGODB_URI = "mongodb://localhost:27017/my-todo-app-db";
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get("/todos", async (req, res) => {
+  const todos = await Todo.find();
+  res.json(todos);
+});
+
+app.post("/todos", async (req, res) => {
+  const todo = new Todo({
+    title: req.body.title,
+    completed: req.body.completed,
+  });
+  await todo.save();
+  res.json(todo);
+});
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
