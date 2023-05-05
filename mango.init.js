4fb93afce13217dbb28445dc96c5804c283106dc
Andrew Willi <andrew.willi@ost.ch>
2023-05-05 20:36:14 +0200

    a

---
+++ mango.init.js
@@ -0,0 +1,7 @@
+db.bar.insertMany([
+    { "id": "bd7b69fa-9207-4996-91cd-b7eec3fce21b", "body": "This is the first entry..", "created": "2020-07-16 18:28:55.933778455 +0000 UTC", "modified": "2020-07-16 18:28:55.933778455 +0000 UTC", "author": "sntshk" },
+    { "id": "8c002a9b-6532-4125-8b58-e2af55a7d60e", "body": "This is the second entry..", "created": "2020-07-16 18:28:55.933778455 +0000 UTC", "modified": "2020-07-16 18:28:55.933778455 +0000 UTC", "author": "sntshk" },
+    { "id": "70032cd2-0c22-41cf-bf02-b77f52dcdb76", "body": "This is the third entry..", "created": "2020-07-16 18:28:55.933778455 +0000 UTC", "modified": "2020-07-16 18:28:55.933778455 +0000 UTC", "author": "sntshk" },
+    { "id": "88d9655b-8364-4062-9153-ad35766d3eb9", "body": "This is the fourth entry..", "created": "2020-07-16 18:28:55.933778455 +0000 UTC", "modified": "2020-07-16 18:28:55.933778455 +0000 UTC", "author": "sntshk" },
+    { "id": "6dddde02-02fa-4027-b469-ab2e3e7dea62", "body": "This is the fifth entry..", "created": "2020-07-16 18:28:55.933778455 +0000 UTC", "modified": "2020-07-16 18:28:55.933778455 +0000 UTC", "author": "sntshk" },
+]);
